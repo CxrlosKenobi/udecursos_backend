256f98ed222efb6e576a3ab112f2955e5f7ee8fb
CxrlosKenobi <kenobi@ieee.org>
2022-08-22 12:44:55 -0400

    Added career data request from Mongo

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask_cors import CORS, cross_origin
 from flask import Flask, jsonify
+#
+from endpoints.Mongo.index import get_career
 
 app = Flask(__name__)
 cors = CORS(app, resources={ r"/*": { "origins": "*" } })
@@ -10,6 +12,13 @@ def index():
   return {
     "status": "okey dokey"
   }
+
+@app.route("/careers/<career_code>", methods=["GET"])
+@cross_origin()
+def get_career(career_code):
+  response = get_career(career_code)
+  return jsonify(response)
+
 
 if __name__ == "__main__":
   app.run(debug=True)
---
+++ endpoints/Mongo/index.py
@@ -0,0 +1,11 @@
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+def get_career(career_code):
+  client = pymongo.MongoClient(os.getenv('MONGO_URI'))
+  db = client.udecursos
+  career = db.careers.find_one({ 'code': career_code })
+  return career
